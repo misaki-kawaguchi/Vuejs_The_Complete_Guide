9178bf603df6d9ec467ec0e4c7e124f3a09db21e
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-26 12:50:55 +0900

    ウォッチャ追加

--- section02/basics-05-using-the-native-event-object/app.js
+++ section02/basics-05-using-the-native-event-object/app.js
@@ -2,18 +2,38 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: '',
+      fullname: ''
     };
   },
+  // 特定のdata(データ)やcomputed(算出プロパティ)の状態を監視して、何か変化があった時に予め登録しておいた処理を自動的に実行してくれる仕組み
+  watch: {
+    // name(newValue, oldValue)
+    name(value) {
+      if (value === '') {
+        this.fullName = '';
+      } else {
+        this.fullName = value + ' ' + this.lastName;
+      }
+    },
+    lastName(value) {
+      if (value === '') {
+        this.fullName = '';
+      } else {
+        this.fullName = this.name + ' ' + value;
+      }
+    },
+  },
   computed: {
-    fullname() {
+    /* fullname() {
       console.log('Runnning again...');
       if (this.name == '') {
         return '';
       } else {
         return this.name + ' ' + 'dummy';
       }
-    }
+    } */
   },
   methods: {
     outputFullname() {
