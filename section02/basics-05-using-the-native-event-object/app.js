5622e458b4a4879788260051a8505e9fba67811a
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-01-26 12:57:33 +0900

    computedに修正

--- section02/basics-05-using-the-native-event-object/app.js
+++ section02/basics-05-using-the-native-event-object/app.js
@@ -4,36 +4,35 @@ const app = Vue.createApp({
       counter: 0,
       name: '',
       lastName: '',
-      fullname: ''
+      /* fullName: '' */
     };
   },
   // 特定のdata(データ)やcomputed(算出プロパティ)の状態を監視して、何か変化があった時に予め登録しておいた処理を自動的に実行してくれる仕組み
   watch: {
     // name(newValue, oldValue)
-    name(value) {
-      if (value === '') {
-        this.fullName = '';
-      } else {
-        this.fullName = value + ' ' + this.lastName;
-      }
-    },
-    lastName(value) {
-      if (value === '') {
-        this.fullName = '';
-      } else {
-        this.fullName = this.name + ' ' + value;
-      }
-    },
+    // name(value) {
+    //   if (value === '') {
+    //     this.fullName = '';
+    //   } else {
+    //     this.fullName = value + ' ' + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === '') {
+    //     this.fullName = '';
+    //   } else {
+    //     this.fullName = this.name + ' ' + value;
+    //   }
+    // },
   },
   computed: {
-    /* fullname() {
+    fullName() {
       console.log('Runnning again...');
-      if (this.name == '') {
+      if (this.name === '' || this.lastName === '') {
         return '';
-      } else {
-        return this.name + ' ' + 'dummy';
-      }
-    } */
+      } 
+      return this.name + ' ' + this.lastName;
+    }
   },
   methods: {
     outputFullname() {
